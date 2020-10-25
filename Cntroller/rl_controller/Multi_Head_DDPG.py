92bc2f14b19a16f06654f234a7c57c18863339ab
wuzy57 <1120902512@qq.com>
2020-10-25 16:17:23 +0800

    0.2

--- Cntroller/rl_controller/Multi_Head_DDPG.py
+++ Cntroller/rl_controller/Multi_Head_DDPG.py
@@ -1,13 +1,43 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import argparse
 from const import *
 from ActorNetwork import *
 from CriticNetwork import *
 
 # new
-args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', default='train', type=str) # mode = 'train' or 'test'
 
+# OpenAI gym environment name, # ['BipedalWalker-v2', 'Pendulum-v0'] or any continuous environment
+# Note that DDPG is feasible about hyper-parameters.
+# You should fine-tuning if you change to another environment.
+
+parser.add_argument("--env_name", default="Pendulum-v0")
+parser.add_argument('--tau',  default=0.01, type=float) # target smoothing coefficient,from const.py:tau = 0.01
+parser.add_argument('--target_update_interval', default=1, type=int)# 更新间隔
+parser.add_argument('--test_iteration', default=10, type=int)# 迭代
+
+parser.add_argument('--learning_rate', default=1e-4, type=float)# from const.py:LRA = 0.00001,LRC = 0.0001
+parser.add_argument('--gamma', default=0.99, type=int) # discounted factor,from const.py:GAMMA = 0.99
+parser.add_argument('--capacity', default=300000, type=int) # replay buffer size，from const.py:REPLAY_BUFFER_SIZE = 300000
+parser.add_argument('--batch_size', default=128, type=int) # mini batch size,from const.py:BATCH_SIZE = 128
+parser.add_argument('--seed', default=false, type=bool)# from const.py:seed = 56
+parser.add_argument('--random_seed', default=56, type=int)
+
+#    optional parameters
+parser.add_argument('--sample_frequency', default=2000, type=int)# 采样频率
+parser.add_argument('--render', default=False, type=bool) # show UI or not
+parser.add_argument('--log_interval', default=50, type=int) # 每50次保存一次模型
+parser.add_argument('--load', default=False, type=bool) # load model
+parser.add_argument('--render_interval', default=100, type=int) # after render_interval, the env.render() will work
+parser.add_argument('--exploration_noise', default=0.1, type=float)
+parser.add_argument('--max_episode', default=20000, type=int) # num of games，from const.py:noise_episode = 20000
+parser.add_argument('--print_log', default=5, type=int)
+parser.add_argument('--update_iteration', default=200, type=int)
+
+args = parser.parse_args()
 class MultiHeadDDPG():
     def load_weights(self, actor_pkl, critic_pkl):
         self.actor.load_state_dict(torch.load(actor_pkl))
